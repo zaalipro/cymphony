728a3c8a4984cad63fb0b66fc228da3f851115b3
Zaali <zaali@live.com>
2026-04-19 21:06:28 +0400

    bump to 0.2.2

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule SymphonyElixir.MixProject do
   def project do
     [
       app: :symphony_elixir,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.19",
       compilers: [:phoenix_live_view] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
